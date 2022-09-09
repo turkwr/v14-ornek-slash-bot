d2586ca9c256b71ce568cb93e9fd167bd6d5f4b2
Türker <63150613+turkwr@users.noreply.github.com>
2022-09-09 10:06:06 +0300

    ✨

---
+++ başlat.bat
@@ -0,0 +1,7 @@
+echo off
+title v14 Ornek Bot (Slash)
+cls
+:x
+node turker.js
+goto :x
+pause
---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  "prefix": "BOT_PREFIX",
+  "owner": "OWNER_ID",
+  "token": "BOT_TOKEN"
+}
